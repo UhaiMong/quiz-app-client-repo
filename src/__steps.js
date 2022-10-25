f37540312b75e7dc85120b8e236ad58de80c51ee
UhaiMong <uhaimarma2016@gmail.com>
2022-10-25 18:12:09 +0600

    meaningfull logo and title added

---
+++ src/__steps.js
@@ -0,0 +1,8 @@
+/**
+ * Questions
+ * 4.1 what is cors?
+ * 4.2 Why are you using firebase? What other options do you have to implement authentication?
+ * 4.3 How does the private route work?
+ * 4.4 What is Node? How does Node work?
+ */
+
